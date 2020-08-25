3f6c4169fe4e17231f1815e33b7e97bad83e2e4c
Berat Bozkurt <beratbozkurt1999@gmail.com>
2020-08-25 13:11:53 +0300

    bug fix

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "36a11fcf4b0cb9c19eee";
+/******/ 	var hotCurrentHash = "7f06398165b5373fb52d";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
